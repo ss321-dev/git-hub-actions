437d88526f830bdaa6d9a9ca4bdfba4472d48ebe
Shinjiro Saito <shin.saito321@gmail.com>
2020-08-14 00:28:51 +0900

    update git hub action

--- tools/dockle_scan.sh
+++ tools/dockle_scan.sh
@@ -22,10 +22,10 @@ dockle_latest=$(
     grep '"tag_name":' | \
     sed -E 's/.*"v([^"]+)".*/\1/' \
 )
-a=$( curl -L -o dockle.deb https://github.com/goodwithtech/dockle/releases/download/v${dockle_latest}/dockle_${dockle_latest}_Linux-64bit.deb )
-b=$( sudo dpkg -i dockle.deb && rm dockle.deb )
 
 scan_result=$(
+    curl -L -o dockle.deb https://github.com/goodwithtech/dockle/releases/download/v${dockle_latest}/dockle_${dockle_latest}_Linux-64bit.deb > dev/null 2>&1
+    sudo dpkg -i dockle.deb && rm dockle.deb > dev/null 2>&1
     dockle -f json ${image_tag}
 )
 
